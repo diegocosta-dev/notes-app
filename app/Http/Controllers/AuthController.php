998e2a1ecf1dc225f255715bc79740d36a79193c
diegocosta-dev <troiablender@gmail.com>
2025-10-14 15:21:15 -0300

    validate login form

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,6 +11,29 @@ class AuthController extends Controller
         return view('login');
     }
 
+    public function loginSubmit(Request $request)
+    {
+        $request->validate(
+            [
+                'text_username' => 'required|email',
+                'text_password' => 'required|min:6|max:16',
+            ],
+            [
+                'text_username.required' => 'Username is required.',
+                'text_username.email' => 'Username must be a valid email address.',
+                'text_password.required' => 'Password is required.',
+                'text_password.min' => 'Password must be at least :min characters.',
+                'text_password.max' => 'Password must not exceed :max characters.',
+            ]
+        );
+
+        $username = $request->input('text_username');
+        $password = $request->input('text_password');
+
+        echo "Username: " . $username . "<br>";
+        echo "Password: " . $password;
+    }
+
     public function logout(Request $request)
     {
         echo "Logout";
--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', [AuthController::class, 'login']);
-Route::get('/logout', [AuthController::class, 'logout']);
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/login-submit', [AuthController::class,'loginSubmit'])->name('login.submit');
+
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
